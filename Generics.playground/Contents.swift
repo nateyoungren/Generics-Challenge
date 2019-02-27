7659cce340c9611ac2ca15beb55b1eae186c638a
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-26 16:14:14 -0800

    fixed the functions to interact with the set.

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,35 +1,53 @@
 import Foundation
 
-struct CountedSet<Element: Hashable> {
+struct CountedSet<Element: Hashable>: ExpressibleByArrayLiteral {
     
-    private var set: [Element] = []
-    
-    mutating func insert(_ element: Element) {
-        set.append(element)
+    init(arrayLiteral elements: Element...) {
+        for element in elements {
+            self.insert(element)
+        }
     }
     
-    mutating func remove() -> Element? {
-        guard !set.isEmpty else { return nil }
-        return set.removeFirst()
+    private var set: [Element: Int] = [:]
+    
+    mutating func insert(_ element: Element) {
+        if let value = set[element] {
+            set[element] = value + 1
+        } else {
+            set[element] = 1
+        }
+    }
+    
+    mutating func remove(_ element: Element) {
+        set[element] = nil
+        
     }
     
     subscript(_ member: Element) -> Int {
-       return member.hashValue
+        return set[member] ?? 0
     }
     
-    func count<Count: Hashable>() -> Count {
-        let uniqueSet: [Element] = []
-        var uniqueNumber = 0
-        if !set.isEmpty {
-            for element in set {
-                if !uniqueSet.contains(element) {
-                    uniqueNumber += 1
-                }
-            }
-            return uniqueNumber as! Count
-        } else {
-            return set.isEmpty as! Count
-        }
+    func count() -> Int? {
+        return set.count
     }
 }
 
+var newCountedSet = CountedSet<String>()
+
+newCountedSet = ["A", "B", "C"]
+newCountedSet.count()
+newCountedSet.remove("A")
+newCountedSet.count()
+newCountedSet.insert("B")
+print(newCountedSet)
+
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet.count()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
